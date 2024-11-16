d624d415c331a7c7cbacf7da2845c7a49f37d03f
adarista012 <ad.arista012@gmail.com>
2024-11-15 22:18:01 -0400

    [feature][add getx package to state management and routing]

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:grape_platinum_card/app/app_colors.dart';
-import 'package:grape_platinum_card/app/presentation/home/home_page.dart';
+import 'package:grape_platinum_card/app/app_constants.dart';
+import 'package:grape_platinum_card/app/app_navigation.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return GetMaterialApp(
+      title: AppConstants.APP_NAME,
+      getPages: AppPages.routes,
+      initialRoute: AppPages.INITIAL,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.main),
-        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
