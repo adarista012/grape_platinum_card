076222297beec354743454ac77a45940a413ac70
adarista012 <ad.arista012@gmail.com>
2024-11-15 22:19:06 -0400

    [architecture][new color]

--- lib/app/app_colors.dart
+++ lib/app/app_colors.dart
@@ -4,4 +4,5 @@ abstract class AppColors {
   static const green = Color(0xff64a533);
   static const main = Color(0xff6b439b);
   static const yellow = Color(0xfff6d374);
+  static const white = Colors.white;
 }
