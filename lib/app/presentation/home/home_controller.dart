1274163464bbc7aee3de487d9be5c5ae486d7efd
adarista012 <ad.arista012@gmail.com>
2024-11-18 14:16:45 -0400

    [feature][home paga repositories features]

--- lib/app/presentation/home/home_controller.dart
+++ lib/app/presentation/home/home_controller.dart
@@ -1,6 +1,38 @@
 import 'package:get/get.dart';
+import 'package:grape_platinum_card/app/domain/models/points.dart';
+import 'package:grape_platinum_card/app/domain/repositories/points_repository.dart';
 
 class HomeController extends GetxController {
+  final int _padding = 8;
+  int get padding => _padding;
+
   int _points = 0;
   int get points => _points;
+
+  List<Points> _pointsList = [];
+  List<Points> get pointsList => _pointsList;
+
+  final PointsRepository _pointsRepository;
+
+  HomeController({required PointsRepository pointsRepository})
+      : _pointsRepository = pointsRepository {
+    _init();
+  }
+
+  void _init() {
+    getPointsList();
+  }
+
+  void getPointsList() async {
+    _pointsList = await _pointsRepository.getPoints();
+    calculateTotal();
+    update();
+  }
+
+  void calculateTotal() {
+    for (Points p in _pointsList) {
+      _points += p.points;
+    }
+    update();
+  }
 }
