effa49635eb2556f550e140be4a8794d136471cf
adarista012 <ad.arista012@gmail.com>
2024-11-18 14:17:55 -0400

    [architecture][new page]

--- lib/app/app_navigation.dart
+++ lib/app/app_navigation.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: constant_identifier_names
 
 import 'package:get/get.dart';
+import 'package:grape_platinum_card/app/presentation/add_points/add_points_page.dart';
 import 'package:grape_platinum_card/app/presentation/home/home_page.dart';
 import 'package:grape_platinum_card/app/presentation/splash/splash_page.dart';
 
 abstract class Routes {
+  static const ADD_POINTS = '/add-points';
   static const HOME = '/home';
   static const SPLASH = '/';
 }
@@ -13,6 +15,11 @@ class AppPages {
   static const INITIAL = Routes.SPLASH;
 
   static final routes = [
+    GetPage(
+      name: Routes.ADD_POINTS,
+      page: () => const AddPointsPage(),
+      transition: Transition.rightToLeft,
+    ),
     GetPage(
       name: Routes.HOME,
       page: () => const HomePage(),
