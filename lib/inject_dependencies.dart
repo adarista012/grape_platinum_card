1b558d088b0922b091e09a46344d2f5ac741b399
adarista012 <ad.arista012@gmail.com>
2024-11-18 14:18:43 -0400

    [architecture][inject dependencies]

--- lib/inject_dependencies.dart
+++ lib/inject_dependencies.dart
@@ -1,4 +1,21 @@
+import 'package:get/get.dart';
+import 'package:grape_platinum_card/app/data/repositories_implementations/points_repository_implementation.dart';
+import 'package:grape_platinum_card/app/data/sources/local/points_local_data_source.dart';
+import 'package:grape_platinum_card/app/presentation/add_points/add_points_contoller.dart';
+import 'package:grape_platinum_card/app/presentation/home/home_controller.dart';
+
 Future<void> injectDependencies() async {
   // await GetStorage.init();
   // final box = GetStorage();
+  final localDataSource = Get.put(PointsLocalDataSource());
+
+  final pointsRepository =
+      Get.put(PointsRepositoryImplementation(localDataSource: localDataSource));
+
+  final homecontroller =
+      Get.put(HomeController(pointsRepository: pointsRepository));
+
+  Get.put(AddPointsContoller(
+      pointsRepository: pointsRepository,
+      homeControllerRepository: homecontroller));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:grape_platinum_card/app/app.dart';
+import 'package:grape_platinum_card/inject_dependencies.dart';
 
-void main() {
+void main() async {
+  await injectDependencies();
   runApp(const App());
 }
