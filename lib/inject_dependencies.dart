82ae24e20424cd14375d177bc9d67c6a53a456e8
adarista012 <ad.arista012@gmail.com>
2024-11-18 15:17:11 -0400

    [feature][add get storage package]

--- lib/inject_dependencies.dart
+++ lib/inject_dependencies.dart
@@ -1,13 +1,15 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:grape_platinum_card/app/data/repositories_implementations/points_repository_implementation.dart';
 import 'package:grape_platinum_card/app/data/sources/local/points_local_data_source.dart';
 import 'package:grape_platinum_card/app/presentation/add_points/add_points_contoller.dart';
 import 'package:grape_platinum_card/app/presentation/home/home_controller.dart';
 
 Future<void> injectDependencies() async {
-  // await GetStorage.init();
-  // final box = GetStorage();
-  final localDataSource = Get.put(PointsLocalDataSource());
+  await GetStorage.init();
+  final box = GetStorage();
+
+  final localDataSource = Get.put(PointsLocalDataSource(box: box));
 
   final pointsRepository =
       Get.put(PointsRepositoryImplementation(localDataSource: localDataSource));
